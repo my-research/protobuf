9ee5e30862fb4390babf95fa40550cdac4e3c7f1
dhslrl321 <dhslrl321@gmail.com>
2023-05-14 14:46:51 +0900

    commit

---
+++ src/test/java/com/github/dhslrl321/protobuf/ProtobufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dhslrl321.protobuf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

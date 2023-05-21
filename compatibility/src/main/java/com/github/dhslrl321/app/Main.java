52c0522dbc0a355281114c159ed17ff7f9e131c7
dhslrl321 <dhslrl321@gmail.com>
2023-05-22 00:51:14 +0900

    commit

---
+++ compatibility/src/main/java/com/github/dhslrl321/app/Main.java
@@ -0,0 +1,7 @@
+package com.github.dhslrl321.app;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- compatibility/src/test/java/com/github/dhslrl321/AT.java
+++ compatibility/src/test/java/com/github/dhslrl321/AT.java
@@ -1,4 +1,24 @@
 package com.github.dhslrl321;
 
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.junit.jupiter.api.Test;
+
 public class AT {
+    @Test
+    void name() throws InvalidProtocolBufferException {
+
+        Personal jang = Personal.newBuilder()
+                .setId(123)
+                .setName("jang")
+                .build();
+
+        ByteString bytes = jang.toByteString();
+
+        System.out.println("bytes = " + bytes);
+        // reader
+        Personal personal = Personal.parseFrom(bytes);
+
+        System.out.println("personal = " + personal);
+    }
 }
